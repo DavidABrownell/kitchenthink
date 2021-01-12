a3d9b7911d4573f28ffe12b375916638bc118875
Ink Brownell <davidabrownell@gmail.com>
2021-01-11 18:17:15 -0800

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`
+        title: `Kitchenthink`,
+        description: `Ink's personal website.`,
+        author: `Ink Brownell`,
+        siteUrl: "https://kitchenth.ink"
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
