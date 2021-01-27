9e8340cc3de40a1c9b990cb6b8e3654fb4f6840f
Ink Brownell <davidabrownell@gmail.com>
2021-01-26 19:16:10 -0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
             options: {
                 name: `blog`,
                 path: `${__dirname}/src/cms/blog`,
+                icon: `${__dirname}/src/images/kitchenthink.svg`,
             },
         },
         `gatsby-transformer-remark`,
@@ -44,6 +45,18 @@ module.exports = {
             },
         },
         `gatsby-plugin-fontawesome-css`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Kitchenthink`,
+                short_name: `KT`,
+                start_url: `/`,
+                background_color: `#ea4836`,
+                theme_color: `#ea4836`,
+                display: `standalone`,
+                icon: `${__dirname}/src/images/kitchenthink.svg`,
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
