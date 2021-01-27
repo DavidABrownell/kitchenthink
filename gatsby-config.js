6a63ba75f4c037b9efba32bf9d63605eb1e5c116
Ink Brownell <davidabrownell@gmail.com>
2021-01-26 18:20:36 -0800

    Add FontAwesone

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
                 modulePath: `${__dirname}/netlify-cms-config.js`,
             },
         },
+        `gatsby-plugin-fontawesome-css`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
