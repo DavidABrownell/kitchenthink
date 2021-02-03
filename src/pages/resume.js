8c8858dbc1935956bbd4023e8206a2a6301c66c6
Ink Brownell <davidabrownell@gmail.com>
2021-02-03 11:53:07 -0800

    Add microformats to resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -10,17 +10,47 @@ const Resume = () => {
                 <SEO title="Résumé" />
 
                 <h1 className="p-name">Ink Brownell</h1>
-                <h2>
-                    <Link to="/contact">Contact</Link>
-                </h2>
+                <div className="p-contact h-card">
+                    <h2>
+                        <Link to="/contact/">Contact</Link>
+                    </h2>
+                    <ul>
+                        <li>
+                            Email:{' '}
+                            <a
+                                href="mailto:ink@softwaredevelopers.coop"
+                                className="u-email"
+                            >
+                                ink@softwaredevelopers.coop
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <h2>Experience</h2>
                 <ul>
-                    <li>
-                        <strong>Meter Group</strong>&mdash;Pullman, WA <br />
-                        <em>
-                            <time dateTime="2019-01">January 2019</time>
+                    <li className="p-experience h-event">
+                        <div className="h-card">
+                            <strong>
+                                <a
+                                    href="https://www.metergroup.com/"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="p-name p-org u-url"
+                                >
+                                    Meter Group
+                                </a>
+                            </strong>
+                            &mdash;<span className="p-locality">Pullman</span>,{' '}
+                            <span className="p-region">WA</span>
+                        </div>
+                        <em className="">
+                            <time dateTime="2019-01" className="dt-start">
+                                January 2019
+                            </time>
                             &ndash;
-                            <time dateTime="2019-08">August 2019</time>
+                            <time dateTime="2019-08" className="dt-end">
+                                August 2019
+                            </time>
                         </em>{' '}
                         <br />
                         Software Engineering Intern
@@ -28,48 +58,99 @@ const Resume = () => {
                 </ul>
                 <h2>Education</h2>
                 <ul>
-                    <li>
-                        <strong>Washington State University</strong>
-                        &mdash;Pullman, WA <br />
-                        <em>
-                            <time dateTime="2018-08">August 2018</time>
-                            &ndash;
-                            <time dateTime="2020-12">December 2020</time>
-                        </em>{' '}
-                        <br />
-                        Bachelor of Science in Computer Science, Minor in
-                        Mathematics
+                    <li className="p-education">
+                        <div className="h-card">
+                            <strong>
+                                <a
+                                    href="https://wsu.edu/"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="p-name p-org u-url"
+                                >
+                                    Washington State University
+                                </a>
+                            </strong>
+                            &mdash;<span className="p-locality">Pullman</span>,{' '}
+                            <span className="p-region">WA</span>
+                        </div>
+                        <div>
+                            <em>
+                                <time dateTime="2018-08" className="dt-start">
+                                    August 2018
+                                </time>
+                                &ndash;
+                                <time dateTime="2020-12" className="dt-end">
+                                    December 2020
+                                </time>
+                            </em>
+                        </div>
+                        <span className="p-summary">
+                            Bachelor of Science in Computer Science,{' '}
+                            <i>magna cum laude</i>
+                        </span>
                     </li>
+                    <li className="p-education">
+                        <div>
+                            <strong>
+                                <a
+                                    href="https://www.bellevuecollege.edu/"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className="p-name p-org u-url"
+                                >
+                                    Bellevue College
+                                </a>
+                            </strong>
+                            &mdash;<span className="p-locality">Bellevue</span>,{' '}
+                            <span className="p-region">WA</span>
+                        </div>
+                        <div>
+                            <em>
+                                <time dateTime="2016-08" className="dt-start">
+                                    August 2016
+                                </time>
+                                &ndash;
+                                <time dateTime="2018-05" className="dt-end">
+                                    May 2018
+                                </time>
+                            </em>
+                        </div>
+                        <span className="p-summary">
+                            Associate of Science in Mechanical Engineering
+                        </span>
+                    </li>
+                </ul>
+                <h2>Awards</h2>
+                <ul>
                     <li>
-                        <strong>Bellevue College</strong>&mdash;Bellevue, WA{' '}
-                        <br />
+                        <div>
+                            <strong>
+                                WSU CptS 423 Fall 2020 Senior Design Competition
+                            </strong>
+                        </div>
                         <em>
-                            <time dateTime="2016-08">August 2016</time>
-                            &ndash;
-                            <time dateTime="2018-05">May 2018</time>
-                        </em>{' '}
-                        <br />
-                        Associate of Science in Mechanical Engineering
+                            1<sup>st</sup> place
+                        </em>
                     </li>
                 </ul>
                 <h2>
-                    <Link to="/projects">Projects</Link>
+                    <Link to="/projects/">Projects</Link>
                 </h2>
                 <h2>Tools & Technologies</h2>
                 <ul style={{ lineHeight: '80%', columnCount: 3 }}>
-                    <li>Python</li>
-                    <li>Django</li>
-                    <li>PostgreSQL</li>
-                    <li>Git</li>
-                    <li>HTML</li>
-                    <li>Bash</li>
-                    <li>CSS</li>
-                    <li>Linux</li>
-                    <li>JavaScript</li>
-                    <li>React.js</li>
-                    <li>MongoDB</li>
-                    <li>Express.js</li>
-                    <li>Gatsby</li>
+                    <li className="p-skill">Python</li>
+                    <li className="p-skill">Django</li>
+                    <li className="p-skill">PostgreSQL</li>
+                    <li className="p-skill">Git</li>
+                    <li className="p-skill">HTML</li>
+                    <li className="p-skill">Bash</li>
+                    <li className="p-skill">CSS</li>
+                    <li className="p-skill">Linux</li>
+                    <li className="p-skill">JavaScript</li>
+                    <li className="p-skill">React.js</li>
+                    <li className="p-skill">MongoDB</li>
+                    <li className="p-skill">Express.js</li>
+                    <li className="p-skill">Gatsby</li>
                 </ul>
             </div>
         </Layout>
